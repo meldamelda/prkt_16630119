7cc3cb4b922e4d5f876c30313c184aa168247ef1
meldamelda <waymelda718@gmail.com>
2020-06-29 11:30:09 +0800

    Tugas praktikum web Melda Yanti 16630119

---
+++ application/views/template/footer.php
@@ -0,0 +1,31 @@
+</div>
+<!-- /#wrapper -->
+
+<!-- jQuery -->
+<script src="<?= base_url('assets/'); ?>js/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="<?= base_url('assets/'); ?>js/bootstrap.min.js"></script>
+
+<!-- Metis Menu Plugin JavaScript -->
+<script src="<?= base_url('assets/'); ?>js/metisMenu.min.js"></script>
+
+<!-- DataTables JavaScript -->
+<script src="<?= base_url('assets/') ?>js/dataTables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url('assets/') ?>js/dataTables/dataTables.bootstrap.min.js"></script>
+
+<!-- Custom Theme JavaScript -->
+<script src="<?= base_url('assets/'); ?>js/startmin.js"></script>
+
+<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<script>
+    $(document).ready(function() {
+        $('#dataTables1').DataTable({
+            responsive: true
+        });
+    });
+</script>
+
+</body>
+
+</html>
